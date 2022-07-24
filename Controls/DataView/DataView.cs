41ac39854c29d3e6f7cf4c04733b3975ec985dc5
KarmaScripter <terry.eppler@gmail.com>
2022-07-24 19:40:31 -0400

    up

--- Controls/DataView/DataView.cs
+++ Controls/DataView/DataView.cs
@@ -9,6 +9,7 @@ namespace BudgetExecution
     using System.Drawing;
     using Syncfusion.Windows.Forms;
     using Syncfusion.Windows.Forms.Grid;
+    using Syncfusion.Windows.Forms.Tools;
 
     /// <summary>
     /// 
@@ -25,21 +26,17 @@ namespace BudgetExecution
             // Control Properties
             ThemesEnabled = true;
             ApplyVisualStyles = true;
-            GridVisualStyles = GridVisualStyles.Office2016Black;
+            TransparentBackground = true;
             EnableAddNew = true;
             EnableEdit = true;
             EnableRemove = true;
             AllowResizeToFit = true;
             ExcelLikeSelectionFrame = true;
             ExcelLikeAlignment = true;
-            AlphaBlendSelectionColor = Color.SteelBlue;
-            Properties.BackgroundColor = Color.FromArgb( 40, 40, 40 );
-            BackColor = Color.FromArgb( 40, 40, 40 );
-            ForeColor = Color.FromArgb( 240, 240, 240 );
             Font = new Font( "Roboto", 9, FontStyle.Regular );
             GridOfficeScrollBars = OfficeScrollBars.Office2010;
             Office2010ScrollBarsColorScheme = Office2010ColorScheme.Black;
-            ColorStyles = ColorStyles.Office2010Black;
+            ColorStyles = ColorStyles.SystemTheme;
             GridVisualStyles = GridVisualStyles.Office2010Black;
             GridLineColor = SystemColors.GradientActiveCaption;
             HorizontalScrollTips = true;
@@ -51,22 +48,11 @@ namespace BudgetExecution
 
             // ThemStyle Properties
             ThemeStyle.CellStyle.Font = new Font( "Roboto", 9, FontStyle.Regular );
-            ThemeStyle.CellStyle.BackColor = Color.FromArgb( 40, 40, 40 );
-            ThemeStyle.CellStyle.TextColor = Color.FromArgb( 240, 240, 240 );
-            ThemeStyle.CheckBoxStyle.BorderColor = Color.FromArgb( 80, 80, 80 );
-            ThemeStyle.CheckBoxStyle.BackColor = Color.FromArgb( 40, 40, 40 );
-            ThemeStyle.CheckBoxStyle.TickColor = Color.Lime;
-            ThemeStyle.ComboBoxStyle.BorderColor = Color.FromArgb( 80, 80, 80 );
-            ThemeStyle.ComboBoxStyle.BackColor = Color.FromArgb( 40, 40, 40 );
-            ThemeStyle.ComboBoxStyle.HoverBackColor = Color.FromArgb( 80, 80, 80 );
-            ThemeStyle.ComboBoxStyle.HoverBorderColor = Color.FromArgb( 1, 115, 199 );
-            ThemeStyle.HeaderStyle.HoverBackColor = Color.FromArgb( 1, 115, 199 ); 
 
             // Model Properties
             Model.Rows.DefaultSize = 22;
             Model.ActiveGridView.PdfExport = true;
             Model.Properties.ThemedHeader = false;
-            Model.Properties.GridLineColor = Color.FromArgb( 64, 64, 64 );
 
             // Style Properties
             Properties.ThemedHeader = true;
@@ -83,7 +69,6 @@ namespace BudgetExecution
             TableStyle.AutoFit = AutoFitOptions.Both;
             TableStyle.Font.Facename = "Roboto";
             TableStyle.Font.Size = 9;
-            TableStyle.BackColor = Color.FromArgb( 40, 40, 40 );
         }
     }
 }
