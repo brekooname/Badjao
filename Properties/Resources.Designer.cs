5ef2c0dbfaa20fefb7392b67fc28b70acc1fe1fc
KarmaScripter <terry.eppler@gmail.com>
2022-07-29 08:19:25 -0400

    up

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -113,6 +113,16 @@ namespace BudgetExecution.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap EditButton {
+            get {
+                object obj = ResourceManager.GetObject("EditButton", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap ExcelButton {
             get {
                 object obj = ResourceManager.GetObject("ExcelButton", resourceCulture);
